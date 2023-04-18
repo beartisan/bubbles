7164f3db16bdc60802154e46bc83df5c3ce0cf2e
Beatrice Tan <joanb.tan@gmail.com>
2023-04-18 18:51:54 -0400

    updated code

---
+++ editButton.js
@@ -0,0 +1,8 @@
+// Get the edit icon element
+const editIcon = document.querySelector('.edit-icon');
+
+// Add an onclick event listener to the edit icon element
+editIcon.addEventListener('click', () => {
+  // Navigate to the edit-profile.html page
+  window.location.href = 'edit-profile.html';
+});
